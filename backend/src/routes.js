3f81d16c4e177916feaea80be0ac40b33e407f0a
Matheus Bastos <matheusbastos@outlook.com>
2019-09-12 08:21:18 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,16 +3,11 @@ const DevController = require('./controllers/DevController');
 const LikeController = require('./controllers/LikeController');
 const DislikeController = require('./controllers/DislikeController');
 
-const CombustivelController = require('./controllers/CombustivelController');
-
 const routes = express.Router();
 
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeController.store);
 routes.post('/devs/:devId/dislikes', DislikeController.store);
-
-
-routes.post('/combustiveis', CombustivelController.store);
 
 module.exports = routes;
